9fa029a9de392b5987d6a92d9909b8afebd1c242
bryandevs1 <rutholuchi89@gmail.com>
2025-01-16 14:43:34 +0100

    hh

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import * as SplashScreen from 'expo-splash-screen'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { useFonts } from 'expo-font'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { FONTS } from './constants/fonts'
 import AppNavigation from './navigations/AppNavigation'
 import { LogBox } from 'react-native'
@@ -9,31 +9,47 @@ import { ThemeProvider } from './theme/ThemeProvider'
 import { BookmarkProvider } from './components/BookmarkContext'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import * as Sentry from '@sentry/react-native'
+import * as Location from 'expo-location'
+import * as Camera from 'expo-camera'
 
 Sentry.init({
     dsn: 'https://a712da57dd7f6ed2482750da9f5e8bc6@o4508652891144192.ingest.us.sentry.io/4508652933218304',
-    // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-    // We recommend adjusting this value in production.
     tracesSampleRate: 1.0,
-    // profilesSampleRate is relative to tracesSampleRate.
-    // Here, we'll capture profiles for 100% of transactions.
     profilesSampleRate: 1.0,
 })
 
-
-// Ignore all log notifications
 LogBox.ignoreAllLogs()
 
 SplashScreen.preventAutoHideAsync()
 
 export default function App() {
     const [fontsLoaded] = useFonts(FONTS)
+    const [locationPermissionGranted, setLocationPermissionGranted] =
+        useState(false)
+    const [cameraPermissionGranted, setCameraPermissionGranted] =
+        useState(false)
+
+    const requestPermissions = async () => {
+        // Request Camera Permission
+        const { status: cameraStatus } =
+            await Camera.requestCameraPermissionsAsync()
+        setCameraPermissionGranted(cameraStatus === 'granted')
+
+        // Request Location Permission
+        const { status: locationStatus } =
+            await Location.requestForegroundPermissionsAsync()
+        setLocationPermissionGranted(locationStatus === 'granted')
+    }
 
     const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
+        if (
+            fontsLoaded &&
+            locationPermissionGranted &&
+            cameraPermissionGranted
+        ) {
             await SplashScreen.hideAsync()
         }
-    }, [fontsLoaded])
+    }, [fontsLoaded, locationPermissionGranted, cameraPermissionGranted])
 
     useEffect(() => {
         const initializeSettings = async () => {
@@ -41,10 +57,17 @@ export default function App() {
             console.log('Face ID Enabled:', faceIDEnabled === 'true')
         }
         initializeSettings()
+
+        // Request permissions as soon as the app is opened
+        requestPermissions()
     }, [])
 
-    // Check fontsLoaded after all hooks have been called
-    if (!fontsLoaded) {
+    // Check if fonts are loaded and permissions are granted
+    if (
+        !fontsLoaded ||
+        !locationPermissionGranted ||
+        !cameraPermissionGranted
+    ) {
         return null
     }
 
